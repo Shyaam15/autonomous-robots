7955546fa9ffa78310c88067bbe86242ae4f046a
Muhammad Showherda Ad-Din <showherda@gmail.com>
2024-06-27 19:52:57 +0800

    Add files via upload

---
+++ cnn_torch.py
@@ -0,0 +1,115 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+import torch.nn.functional as F
+from PIL import Image
+import os
+
+class SignLanguageDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.classes = os.listdir(root_dir)
+        self.images = []
+        self.labels = []
+
+        for i, cls in enumerate(self.classes):
+            cls_path = os.path.join(root_dir, cls)
+            for img_name in os.listdir(cls_path):
+                self.images.append(os.path.join(cls_path, img_name))
+                self.labels.append(i)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path = self.images[idx]
+        image = Image.open(img_path).convert('L')
+        label = self.labels[idx]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 80, kernel_size=5)
+        self.conv2 = nn.Conv2d(80, 80, kernel_size=5)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+        self.batch_norm1 = nn.BatchNorm2d(80)
+        self.batch_norm2 = nn.BatchNorm2d(80)
+
+        self.fc1 = nn.Linear(1280, 250)
+        self.fc2 = nn.Linear(250, 25)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.batch_norm1(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = self.batch_norm2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
+
+        return x
+
+transform = transforms.Compose([
+    transforms.Resize((28, 28)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+train_dataset = SignLanguageDataset(root_dir='~/Desktop/sign_language_translation/data/train', transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+val_dataset = SignLanguageDataset(root_dir='~/Desktop/sign_language_translation/data/test', transform=transform)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+model = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+num_epochs = 20
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for images, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+    print('Epoch [{}/{}], Loss: {}'.format(epoch+1, num_epochs, running_loss/len(train_loader)))
+
+    # Validation
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in val_loader:
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Validation Accuracy: {} %'.format(100 * correct / total))
+
+torch.save(model.state_dict(), 'model_trained.pt')

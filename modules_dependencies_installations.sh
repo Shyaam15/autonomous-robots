680540fd6d022c5d3a2c16eb5570bdf7f0c54e99
Muhammad Showherda Ad-Din <showherda@gmail.com>
2024-06-28 21:00:00 +0800

    Update modules_dependencies_installations.sh

--- modules_dependencies_installations.sh
+++ modules_dependencies_installations.sh
@@ -12,9 +12,8 @@ sudo apt-get install -y python-pip
 # Install the specific versions of required modules
 pip install pyaudio==0.2.11
 pip install SpeechRecognition==3.8.1
-pip install futures==1.0.0 
+pip install future==1.0.0 
 pip install numpy==1.16.6
 pip install torch==1.4.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html 
-pip install opencv-python==3.2.0
 
 echo "Installation completed successfully."

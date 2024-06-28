0caeaca23305d1ec8116085cf62720928a11fe3f
Muhammad Showherda Ad-Din <showherda@gmail.com>
2024-06-28 21:09:29 +0800

    Update modules_dependencies_installations.sh

--- modules_dependencies_installations.sh
+++ modules_dependencies_installations.sh
@@ -10,10 +10,10 @@ sudo apt-get install -y portaudio19-dev python-all-dev
 sudo apt-get install -y python-pip
 
 # Install the specific versions of required modules
-pip install pyaudio==0.2.11
-pip install SpeechRecognition==3.8.1
-pip install future==1.0.0 
-pip install numpy==1.16.6
-pip install torch==1.4.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html 
+pip2 install pyaudio==0.2.11
+pip2 install SpeechRecognition==3.8.1
+pip2 install future==1.0.0 
+pip2 install numpy==1.16.6
+pip2 install torch==1.4.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html 
 
 echo "Installation completed successfully."
